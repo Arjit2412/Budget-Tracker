c725b74598ae57f1b97aa3a0fa0a1a07a61ab374
Ashu463 <ashukasaudhan971@gmail.com>
2025-01-28 00:01:02 +0530

    fix: iteration#2

--- src/app/api/income/[id].js
+++ src/app/api/income/[id].js
@@ -5,44 +5,12 @@ export default async function handler(req, res) {
   try {
     if (req.method === "GET") {
       // Fetch income by ID
-      const state = req.headers['state'];
-      const local = req.headers['local'];
-      const name = req.headers['name'];
-      const id = req.headers['id'];
+      const state = req.param['state'];
+      const local = req.param['local'];
+      const ministry = req.param['ministry'];
+      const id = req.param['id'];
 
-      if(state){
-        const state = await prisma.state.findUnique({
-          where: { state: state },
-        });
-        if (!state) {
-          return res.status(404).json({ error: "State not found" });
-        }
-        return res.status(200).json(state);
-      }
-      else if(local){
-        const local = await prisma.local.findUnique({
-          where: { local: local },
-        });
-        if (!local) {
-          return res.status(404).json({ error: "Local not found" });
-        }
-        return res.status(200).json(local);
-      }
-      else if(name){
-        const income = await prisma.income.findFirst({
-          where: { name: name },
-          include: {
-            expenditure: true,
-            state: true,
-            local: true,
-          },
-        });
-        if (!income) {
-          return res.status(404).json({ error: "Income not found" });
-        }
-        return res.status(200).json(income);
-      }
-      else if(id){
+      if(id){
         const income = await prisma.income.findUnique({
           where: { iid: id },
           include: {
@@ -56,6 +24,43 @@ export default async function handler(req, res) {
         }
         return res.status(200).json(income);
       }
+
+      if(ministry){
+        const income = await prisma.income.findMany({
+          where: { ministryMid: ministry },
+          include: {
+            expenditure: true,
+            state: true,
+            local: true,
+          },
+        });
+        if (!income) {
+          return res.status(404).json({ error: "Income not found" });
+        }
+        return res.status(200).json(income);
+      }
+
+      if(local){
+        const local = await prisma.local.findMany({
+          where: { local: local },
+        });
+        if (!local) {
+          return res.status(404).json({ error: "Local not found" });
+        }
+        return res.status(200).json(local);
+      }
+
+      if(state){
+        const state = await prisma.state.findMany({
+          where: { state: state },
+        });
+        if (!state) {
+          return res.status(404).json({ error: "State not found" });
+        }
+        return res.status(200).json(state);
+      }
+
+
     } else if (req.method === "PUT") {
       // Update income by ID
       const { name, desc, date, t_amount, expenditure, central, state, local } =
